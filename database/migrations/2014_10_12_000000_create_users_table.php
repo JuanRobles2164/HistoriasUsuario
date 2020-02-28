cec6be52862ef2eba3b52f3fcb69abaf3fbcc844
Juan Esteban Robles <juanestebanrobles3103@gmail.com>
2020-02-28 15:26:32 -0500

    implementación de login y migrates
    El sistema ya cuenta con un sistema de Login, registro, validación, recuperación de contraseña y cambio de contraseña para cada usuario
    Ahora, sólo con escribir en la consola del VS Code "php artisan migrate" generará la estructura de la base de datos correspondiente
    Para no andar jodiendo de un lado a otro con ese berraco archivo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,22 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        Schema::create('role', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nombre');
+        });
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('user_role_fk');
             $table->rememberToken();
             $table->timestamps();
+            
+            $table->foreign('user_role_fk')
+                ->references('id')->on('role');
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/', function () {
 
 // {CONTROLLER}/{METHOD}/{ID?}
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
