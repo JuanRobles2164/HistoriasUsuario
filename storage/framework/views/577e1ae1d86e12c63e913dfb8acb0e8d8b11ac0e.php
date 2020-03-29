55f0c269541d7a4a41367a286a95ea2c5944bd41
gabocastro29 <55069295+gabocastro29@users.noreply.github.com>
2020-03-29 16:07:10 -0500

    vista de nuevo usuario
    se corrigió la vista inicial para la creación de un nuevo usuario

--- storage/framework/views/577e1ae1d86e12c63e913dfb8acb0e8d8b11ac0e.php
+++ storage/framework/views/577e1ae1d86e12c63e913dfb8acb0e8d8b11ac0e.php
@@ -1,30 +1,57 @@
 <?php $__env->startSection('contenido'); ?>
     <form action="<?php echo e(route('admin.postCreate')); ?>" method="POST">
         <?php echo csrf_field(); ?>
-        <div class="form-group">
+        <div class="container-fluid">
+            <form class="from-neon" autocomplete="off">
+                <fieldset>
+                    <legend><i class="fas fa-user"></i> &nbsp; Información básica</legend>
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div cass="col-12 col-md-6">
+                                <div class="from-group">
+                                    <label for="usuario_nombre">Nombres</label>
+                                    <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,45}" class="form-control" name="usuario_nombre" id="usuario_nombre" maxlength="45">
+                                </div>
 
-            <label for="">Nombres</label>
-            <input type="text" name="nombres">
+                                <div class="from-group">
+                                    <label for="usuario_apellido">Apellidos</label>
+                                    <input type="text" pattern="[a-zA-ZáéíóúÁÉÍÓÚñÑ ]{1,45}" class="form-control" name="usuario_apellido" id="usuario_apellido" maxlength="45">
+                                </div>
 
-            <label for="">Apellidos</label>
-            <input type="text" name="apellidos">
+                                <div class="from-group">
+                                    <label for="usuario_cc">Identificacion</label>
+                                    <input type="text" pattern="[a-zA-Z0-9-]{1,35}" class="form-control" name="usuario_cc" id="usuario_cc" maxlength="35">
+                                </div>
 
-            <label for="">Correo electrónico</label>
-            <input type="text" name="email">
+                                <div class="from-group">
+                                    <label for="usuario_email">Correo electrónico</label>
+                                    <input type="email" class="form-control" name="usuario_email" id="usuario_email" maxlength="70">
+                                </div>
 
-            <label for="">Identificacion</label>
-            <input type="text" name="identificacion">
+                                <div class="from-group">
+                                    <label for="usuario_password">Contraseña</label>
+                                    <input type="password" class="form-control" name="contrasenia" id="contrasenia" maxlength="150">
+                                </div>
 
-            <label for="">Contraseña</label>
-            <input type="text" name="clave">
-            
-            <label for="">Seleccione el rol:</label>
-            <select name="rol" id="">
-                <?php $__currentLoopData = $roles; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rol): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-                    <option value="<?php echo e($rol->id); ?>"> <?php echo e($rol->abreviatura); ?></option>
-                <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-            </select>
-            <button type="submit">CREAR</button>
+                                <div class="form-group">
+                                    <label for="usuario_rol">Seleccioné el Rol</label>
+                                    <select class="form-control" name="usuario_rol">
+                                        <option hidden>Seleccione una opción</option>
+                                        <?php $__currentLoopData = $roles; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rol): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                            <option value="<?php echo e($rol->id); ?>"><?php echo e($rol->abreviatura); ?></option>    
+                                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                        
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </fieldset>
+                <p class="text-center" style="margin-top: 40px;">
+                    <button type="reset" class="btn btn-raised btn-secondary btn-sm"><i class="fas fa-paint-roller"></i> &nbsp; LIMPIAR</button>
+                    <button type="submit" class="btn btn-raised btn-info btn-sm"><i class="far fa-save"></i> &nbsp; GUARDAR</button>
+                </p>
+            </form>
         </div>
     </form>
 <?php $__env->stopSection(); ?>
