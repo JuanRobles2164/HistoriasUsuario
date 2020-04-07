01e7659aa33aa993ebfd6f0633cf2d2236de46c2
gabocastro29 <55069295+gabocastro29@users.noreply.github.com>
2020-04-07 14:38:30 -0500

    vista de crear proyecto
    se agrego la vista respectiva para la creacion de proyecto y se revisa que los campos vacios esten llenos

--- resources/views/Contents/Docente/crearProyecto.blade.php
+++ resources/views/Contents/Docente/crearProyecto.blade.php
@@ -3,28 +3,46 @@
 <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
   <link rel="stylesheet" href="/resources/demos/style.css">
     <form action="{{route('docente.postCrearProyecto')}}" method="POST">
-        @csrf
-        <label for="">Nombre</label>
-        <input type="text" name="nombre">
-        <br>
-        <label for="">Descripcion</label>
-        <input type="text" name="descripcion">
-        <br>
-        <label for="fecha_limite">Fecha inicial:</label>
-        <input type="date" name="fecha_inicial">
-        <br>
-        <label for="fecha_limite">Fecha límite:</label>
-        <input type="date" name="fecha_limite">
-        <br>
-        <label for="">Metodologia</label>
-        <select name="id_metodologia" id="">
-            @foreach ($metodologias as $metodologia)
-                <option value="{{$metodologia->id}}">{{$metodologia->nombre}}</option>
-            @endforeach
-        </select>
-        <br>
-        <input type="hidden" name="id_estado" value="1">
-        <br>
-        <button type="submit" class="btn btn-primary">Crear</button>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+              <h2 class="display-4"> <i class="fas fa-project-diagram"></i> Nuevo Proyecto</h2>
+                <form>
+                    @csrf
+                    <div class="form-row">
+                        <div class="col-md-4 mb-3">
+                          <label for="validationCustom01">Nombre</label>
+                          <input type="text" class="form-control" name="nombre" required>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="validationCustom02">Descripción</label>
+                            <input type="text" class="form-control" name="descripcion" required>
+                        </div>
+                    </div>
+                    <div class="form-row">
+                        <div class="col-md-3 mb-3">
+                            <label for="validationCustom03">Fecha inicial:</label>
+                            <input type="date" class="form-control" name="fecha_inicial" required>
+                        </div>
+                        <div class="col-md-3 mb-3">
+                            <label for="validationCustom04">Fecha límite:</label>
+                            <input type="date" class="form-control" name="fecha_limite" required>
+                        </div>
+                        <div class="col-md-3 mb-3">
+                            <label for="validationCustom05">Metodología</label>
+                            <select class="custom-select" id="validationDefault05" name="id_metodologia" required>
+                                @foreach ($metodologias as $metodologia)
+                                    <option value="{{$metodologia->id}}">{{$metodologia->nombre}}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                    <input type="hidden" name="id_estado" value="1">
+                    <br>
+                    <p class="text-center">
+                    <button type="submit" class="btn btn-primary btn-lg">Crear</button>
+                    </p>
+                </form>          
+            </div>
+        </div>
     </form>
 @endsection
