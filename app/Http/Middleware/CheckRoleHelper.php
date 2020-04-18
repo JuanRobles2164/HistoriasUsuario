e5e56125c97a5a5e493209e9699509cac4df9dc6
Juan Esteban Robles <juanestebanrobles3103@gmail.com>
2020-04-18 18:28:50 -0500

    Update CheckRoleHelper.php
    aún se intenta implementar el filtrado de navegación acorde al rol del usuario

--- app/Http/Middleware/CheckRoleHelper.php
+++ app/Http/Middleware/CheckRoleHelper.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Exception;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Contracts\Encryption\Encrypter;
@@ -10,11 +11,10 @@ class CheckRoleHelper {
     public static function checkCookieRole($request, $route) : bool
     {
         if($request->hasCookie('usuario')){
-            $encrypter = app(\Illuminate\Contracts\Encryption\Encrypter::class);
+            //$encrypter = app(\Illuminate\Contracts\Encryption\Encrypter::class);
             $usuario = Cookie::get('usuario');
             //$usuario = $encrypter->decrypt(Cookie::get('usuario'));
-            //$usuarioRol =  Crypt::decrypt($usuario);
-            $usuarioRol = $encrypter->decrypt($usuario, false);
+            $usuarioRol =  Crypt::decrypt(Cookie::get('usuario'), false);
             if(strpos($route, strtolower($usuarioRol)) != null){
                 return true;
             }
