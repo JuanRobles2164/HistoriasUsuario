3eb0a8094cf7a7c16e100d3e19a7ab7085f94fb7
Juan Esteban Robles <juanestebanrobles3103@gmail.com>
2020-04-17 22:20:13 -0500

    Renderización de blade automática
    Se suben las vistas que el blade está renderizando

--- storage/framework/views/4e24af0ded4ecc96858f8bf543f58cc29f0bb849.php
+++ storage/framework/views/4e24af0ded4ecc96858f8bf543f58cc29f0bb849.php
@@ -4,25 +4,22 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
       <title>Inicio</title>
-         <!-- Normalize V8.0.1 -->
-         <link rel="stylesheet" href="<?php echo e(URL::asset('CSS/normalize.css')); ?>">
-         <!-- Bootstrap V4.3 -->
-         <link rel="stylesheet"  href="<?php echo e(URL::asset('CSS/bootstrap.min.css')); ?>">
-         <!-- Font Awesome V5.9.0 -->
-         <link rel="stylesheet"  href="<?php echo e(URL::asset('CSS/all.css')); ?>">
-         <!-- Sweet Alerts V8.13.0 CSS file -->
-         <link rel="stylesheet"  href="<?php echo e(URL::asset('CSS/sweetalert2.min.css')); ?>">
-         <!-- jQuery Custom Content Scroller V3.1.5 -->
-         <link rel="stylesheet" href="<?php echo e(URL::asset('CSS/jquery.mCustomScrollbar.css')); ?>">
-         <!-- Sweet Alert V8.13.0 JS file-->
-         <script src="<?php echo e(URL::asset('JS/sweetalert2.min.js')); ?>" ></script>
-         <!-- General Styles -->
-         <link rel="stylesheet" href="<?php echo e(URL::asset('CSS/StyleDocente.css')); ?>">
-         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+      <!-- jQuery V3.4.1 -->
+      <script src="<?php echo e(URL::asset('JS/jquery-3.4.1.min.js')); ?>"></script>
+      <!-- Normalize V8.0.1 -->
+      <link rel="stylesheet" href="<?php echo e(URL::asset('CSS/normalize.css')); ?>">
+      <!-- Bootstrap V4.3 -->
+      <link rel="stylesheet" href="<?php echo e(URL::asset('CSS/bootstrap.min.css')); ?>">
+      <!-- Font Awesome V5.9.0 -->
+      <link rel="stylesheet" href="<?php echo e(URL::asset('CSS/all.css')); ?>">
+      <!-- Sweet Alerts V8.13.0 CSS file -->
+      <link rel="stylesheet" href="<?php echo e(URL::asset('CSS/sweetalert2.min.css')); ?>">
+      <!-- jQuery Custom Content Scroller V3.1.5 -->
+      <link rel="stylesheet" href="<?php echo e(URL::asset('CSS/jquery.mCustomScrollbar.css')); ?>">
+      <!-- General Styles -->
+      <link rel="stylesheet" href="<?php echo e(URL::asset('CSS/styleDocente.css')); ?>">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
    </head>
    <body>
       <!-- centenedor de menu -->
@@ -33,7 +30,7 @@
             <div class="full-box nav-lateral-content">
                <figure class="full-box nav-lateral-avatar">
                   <i class="far fa-times-circle show-nav-lateral"></i>
-                  <img src="<?php echo e(URL::asset('Images/Icons/avatar-3.svg')); ?>" class="img-fluid" alt="Avatar">
+                  <img src="<?php echo e(URL::asset('Images/Icons/avatar-3.Svg')); ?>" class="img-fluid" alt="Avatar">
                   <figcaption class="roboto-medium text-center">
                      <?php echo e(json_decode(Crypt::decrypt((Cookie::get('usuario'))))->nombre); ?>
 
@@ -96,6 +93,8 @@
             </footer>
          </section>
       </main>
+      <!-- Sweet Alert V8.13.0 JS file-->
+      <script src="<?php echo e(URL::asset('JS/sweetalert2.min.js')); ?>"></script>
       <!-- jQuery V3.4.1 -->
       <script src="<?php echo e(URL::asset('JS/jquery-3.4.1.min.js')); ?>"></script>
       <!-- popper -->
@@ -108,9 +107,10 @@
       <script src="<?php echo e(URL::asset('JS/bootstrap-material-design.min.js')); ?>" ></script>
       <script src="<?php echo e(URL::asset('JS/main.js')); ?>"></script>
       <script src="<?php echo e(URL::asset('JS/AJAX/DocenteAJAX.JS')); ?>"></script>
-      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+      <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
    </body>
 </html>
 <?php /**PATH C:\xampp\htdocs\paginasPHP\HistoriasUsuario\resources\views/Templates/Docente/_LayoutDocente.blade.php ENDPATH**/ ?>
