84489bc51a568ad278c3852b1738c17059bbc615
MarlyAcosta <marlyacosta.a3@gmail.com>
2020-05-22 02:28:41 -0500

    paso de scripts sql a orm Docente primera parte

--- app/Http/Daos/DocenteDao.php
+++ app/Http/Daos/DocenteDao.php
@@ -34,24 +34,29 @@ class DocenteDao extends Controller
         return $proyecto;
     }
     public static function eliminarFuenteMetodologia($id){
-        $SQL = "DELETE FROM fuente WHERE id = $id";
-        DB::delete($SQL);
+        DB::table('fuente')->where('id', $id)->delete();
     }
     public static function crearMetodologia(metodologia $metodologia){
-        $SQL = "INSERT INTO metodologia(nombre, descripcion, estado_eliminado) VALUES ('$metodologia->nombre', '$metodologia->descripcion', 0)";
-        DB::insert($SQL);
+        $id = DB::table('metodologia')->insertGetId(
+            array('nombre' => $metodologia->nombre, 'descripcion' => $metodologia->descripcion, 'estado_eliminado' => 0)
+        );
     }
     public static function agregarFuenteMetodologia($fuente){
-        $SQL = "INSERT INTO fuente(url, descripcion, id_metodologia, creado_en, modificado_en) VALUES('$fuente->url', '$fuente->descripcion', $fuente->id_metodologia, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)";
-        DB::insert($SQL);
+        $id = DB::table('fuente')->insertGetId(
+            array('url' => $fuente->url, 'descripcion' => $fuente->descripcion, 'id_metodologia' => $fuente->id_metodologia, 'creado_en' => Utilities::getCurrentDate(), 'modificado_en' => Utilities::getCurrentDate())
+        );
     }
     public static function eliminarMetodologia($id){
-        $SQL = "UPDATE metodologia SET estado_eliminado = 1 WHERE id='$id'";
-        DB::update($SQL);
+        DB::table('metodologia')
+        ->where('id',$id)
+        ->update(array('estado_eliminado' => 1));
+        //$SQL = "UPDATE metodologia SET estado_eliminado = 1 WHERE id='$id'";
+        //DB::update($SQL);
     }
     public static function editarMetodologia(metodologia $metodologia){
-        $SQL = "UPDATE metodologia SET nombre='$metodologia->nombre', descripcion='$metodologia->descripcion', updated_at = CURRENT_TIMESTAMP WHERE id='$metodologia->id'";
-        DB::update($SQL);
+        DB::table('metodologia')
+        ->where('id',$metodologia->id)
+        ->update(array('nombre' => $metodologia->nombre, 'descripcion' => $metodologia->descripcion, 'updated_at' => Utilities::getCurrentDate()));
     }
     public static function getAllProyectos($id_docente){
         $proyectos = DB::select("SELECT p.*,(SELECT DATEDIFF(p.fecha_limite, p.fecha_inicial)) AS dias_restantes FROM proyecto p WHERE p.id_usuario = $id_docente ORDER BY id_estado ASC");
