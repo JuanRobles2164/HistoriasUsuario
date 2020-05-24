4db61419c5638808a77934bf044f962e8e45ee21
Juan Esteban Robles <juanestebanrobles3103@gmail.com>
2020-05-23 22:24:02 -0500

    Arquitectura controllers y DAOs
    - Se reestandarizan algunas configuraciones para los DAOs y el controller maestro

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,6 +10,10 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    protected static $JSON_SUCCESS = array('success' => true);
+    protected static $JSON_FAIL = array('success' => false);
+    protected static $ACTIVO = true;
+    protected static $INACTIVO = false;
     protected function verifyRole(){
         
     }
