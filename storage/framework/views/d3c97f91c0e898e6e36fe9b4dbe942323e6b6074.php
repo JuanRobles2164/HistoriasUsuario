abbe12e8211da34f3210959a8ab283689d592769
Juan Esteban Robles <juanestebanrobles3103@gmail.com>
2020-05-28 01:06:58 -0500

    Renderizado de blade / Plantilla maestra Alumno
    - Se le agrega la posibilidad de usar ToolTips en algunos botones

--- storage/framework/views/d3c97f91c0e898e6e36fe9b4dbe942323e6b6074.php
+++ storage/framework/views/d3c97f91c0e898e6e36fe9b4dbe942323e6b6074.php
@@ -104,6 +104,11 @@
       </script>
       <script src="<?php echo e(URL::asset('JS/main.js')); ?>"></script>
       <?php echo $__env->yieldContent('custom_scripts'); ?>
+      <script>
+         $(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+         })
+      </script>
       <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
    </body>
