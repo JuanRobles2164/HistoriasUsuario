5f4779c38a8e2a8790e0fb19a20280afd423db5e
MarlyAcosta <marlyacosta.a3@gmail.com>
2020-06-01 17:06:13 -0500

    Eliminacion de vistas que se reemplazaron con ajax
    Se eliminaron las vistas las cuales ya fueron reemplazadas con su correspondiente ajax

--- resources/views/Contents/Alumno/crearRecurso.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('/Templates/Alumno/_LayoutAlumno')
-@section('contenido')
-    {{-- vista vacia se remplazo con un modal --}}
-@endsection
-    
--- resources/views/Contents/Alumno/editarModulo.blade.php
+++
@@ -1,4 +0,0 @@
-@extends('/Templates/Alumno/_LayoutAlumno')
-@section('contenido')
-{{-- vista vacia se remplazo con un modal --}}
-@endsection
