6cd93a635615e404870cce87a76108d231286cd8
gabocastro29 <55069295+gabocastro29@users.noreply.github.com>
2020-06-18 00:03:17 -0500

    correcion de observaciones
    se corrio el error de la vista de objservaciones

--- resources/views/Contents/Docente/listaMetodologias.blade.php
+++ resources/views/Contents/Docente/listaMetodologias.blade.php
@@ -4,38 +4,40 @@
 <h3>Lista de metodologías</h3>
 <br>
 <div style="text-align:center">
-   <table class="table table-hover">
+   <table class="table">
       <thead class="bg-primary">
          <tr>
-            <th scope="col" valign="middle" style="text-align: center">Nombre</th>
-            <th scope="col" valign="middle" style="text-align: center">Descripción</th>
-            <th scope="col" valign="middle" style="text-align: center">Estado</th>
-            <th scope="col" valign="middle" style="text-align: center">Acciones</th>
+            <td scope="col" valign="middle" style="text-align: center">Nombre</td>
+            <td scope="col" valign="middle" style="text-align: center">Descripción</td>
+            <td scope="col" valign="middle" style="text-align: center">Estado</td>
+            <td scope="col" valign="middle" style="text-align: center">Acciones</td>
          </tr>
       </thead>
-      @foreach ($metodologias as $metodologia)
-         <tr style="border-color: black; border-radius: 1px; vertical-align:middle; height:100%">
-            <td scope="row" valign="middle">{{$metodologia->nombre}}</td>
-            <td scope="row" valign="middle">{{$metodologia->descripcion}}</td>
-            @if($metodologia->estado_eliminado == 0)
-               <td scope="row" valign="middle">Activa</td>
-            @else
-               <td scope="row" valign="middle">Eliminada</td>
-            @endif
-            <td scope="row" valign="middle">
-               <a class="btn btn-info btn-sm" style="color: white;" onclick="consultandometodologia({{$metodologia->id}})">
-                  <i class="fas fa-eye"></i>
-               </a>
-               <a href="{{route('docente.getEditarMetodologia', 'id='.$metodologia->id)}}" a class="btn btn-success btn-sm">
-                  <i class="far fa-edit"></i>
-               </a>
-               <a href="#" class="btn btn-danger btn-sm">
-                  <i class="far fa-trash-alt"></i>
-               </a>
-            </td>
-         </tr>
-      @endforeach
-   </table>
+      <tbody>
+         @foreach ($metodologias as $metodologia)
+            <tr style="border-color: black; border-radius: 1px; vertical-align:middle; height:100%">
+               <td scope="row" valign="middle">{{$metodologia->nombre}}</td>
+               <td scope="row" valign="middle">{{$metodologia->descripcion}}</td>
+               @if($metodologia->estado_eliminado == 0)
+                  <td scope="row" valign="middle">Activa</td>
+               @else
+                  <td scope="row" valign="middle">Eliminada</td>
+               @endif
+               <td scope="row" valign="middle">
+                  <a class="btn btn-info btn-sm" style="color: white;" onclick="consultandometodologia({{$metodologia->id}})">
+                     <i class="fas fa-eye"></i>
+                  </a>
+                  <a href="{{route('docente.getEditarMetodologia', 'id='.$metodologia->id)}}" a class="btn btn-success btn-sm">
+                     <i class="far fa-edit"></i>
+                  </a>
+                  <a href="#" class="btn btn-danger btn-sm">
+                     <i class="far fa-trash-alt"></i>
+                  </a>
+               </td>
+            </tr>
+         @endforeach
+      </tbody>
+    </table>
 </div>
 @endsection
 
